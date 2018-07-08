ff94a209f1c516140cf07ddda7c3bcdd9dc16201
geethna <31569222+geethna@users.noreply.github.com>
2018-07-08 10:57:10 +0530

    Update density.py

--- density.py
+++ density.py
@@ -1,3 +1,7 @@
+#The challenge is from ASIS-QUALS(2018)
+#The given nam of the binary was b64pack. So I use the command "base64 <filename>" and I got the string "b64dec" given below.
+#Then after reversing I wrote the following script to print the flag.
+
 b64dec="O++h+b+qcASIS++e01d+c4Nd+cGoLD+cASIS+c1De4+c4H4t+cg0e5+cf0r+cls+d++gdI++j+kM+vb++fD9W+q/Cg=="
 new=""
 l=len(b64dec)
